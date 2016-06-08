14a00992fc69a2fd2e3d2b328c01b9cd7e0ddebb
Grisanu Naing <mgrisanu@gmail.com>
2016-06-08 10:31:14 -0700

    (feat) Start working on adding Practice view
    -Make practice-view folder
          -populate with appropiate jsx files
    -Re-route HomeView.jsx to practice-view
    -Change App.jsx routing to practice-view
    	-nest session-view inside

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,7 @@ import MainLayout from './main-layout/MainLayout.jsx';
 import HomeView from './home-view/HomeView.jsx';
 import SetupView from './setup-view/SetupView.jsx'; 
 import RecordView from './record-view/RecordView.jsx';
+import PracticeView from './practice-view/PracticeView.jsx';
 import SessionsView from './sessions-view/SessionsView.jsx';
 import ReportView from './report-view/ReportView.jsx';
 import SettingsView from './settings-view/SettingsView.jsx';
@@ -21,8 +22,10 @@ export default class App extends React.Component {
         <Route path="/" component={MainLayout}>
           <IndexRoute component={HomeView} />
           <Route path="setup" component={SetupView} />
-          <Route path="record/:practiceId" component={RecordView} />
-          <Route path="sessions" component={SessionsView} />
+          <Route path="record" component={RecordView} />
+          <Route path="practices" component={PracticeView}>
+            <Route path="sessions" component={SessionsView} />
+          </Route>
           <Route path="reports/:sessionId" component={ReportView} />
           <Route path="settings" component={SettingsView} />
         </Route>
