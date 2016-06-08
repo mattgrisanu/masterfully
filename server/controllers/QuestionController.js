2cd4a35f7bb708ee38f7e34171f670ae29320189
Grisanu Naing <mgrisanu@gmail.com>
2016-06-07 18:47:27 -0700

    (feat) Add controller file for Practice and Quetion table

---
+++ server/controllers/QuestionController.js
@@ -0,0 +1,1 @@
+var Question = require('../models/QuestionModel.js');
--- server/models/PracticeModel.js
+++ server/models/PracticeModel.js
@@ -4,8 +4,8 @@ var Question = require('./QuestionModel.js');
 
 db.knex.schema.hasTable('practices').then(function(exists){
   if(!exists) {
-    db.knex.schema.createTable('practices', function(pratice) {
-      practice.increment('id').primary();
+    db.knex.schema.createTable('practices', function(practice) {
+      practice.increments('id').primary();
       practice.string('name', 100);
       practice.string('description', 255);
       practice.integer('user_id').unsigned().references('users.id');
@@ -21,7 +21,7 @@ var Practice = db.Model.extend({
   hasTimestamps: true,
   user: function() {
     return this.belongsTo(User, 'user_id');
-  }
+  },
   questions: function() {
     return this.hasMany(Question);
   }
