00e0a300e65dcfb869db70551ac26aa6828b8c5b
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-06-10 14:20:29 -0700

    feat-Speech is translated to text and analyzed"

--- server/controllers/SpeechController.js
+++ server/controllers/SpeechController.js
@@ -1,40 +1,40 @@
 var Speech = require('../models/SpeechModel.js');
 var watson = require('watson-developer-cloud'); 
 
+var _analyzeTone = function (speech, callback) {
+  var tone_analyzer = watson.tone_analyzer({
+    url: "https://gateway.watsonplatform.net/tone-analyzer/api",
+    username: '67de49fe-9c37-4cf6-9ebb-f0e5be00533d',
+    password: 'mz5qSSNJQupj',
+    version: 'v3',
+    version_date: '2016-05-19 '
+  });
+
+  tone_analyzer.tone({text: speech}, function (err, tone) {
+    if (err) {
+      console.log(err);
+      callback(err); 
+    } else {
+      res = JSON.stringify(tone, null, 2)
+      console.log(res); 
+      callback(null, res); 
+    }
+  })
+}
 
 module.exports = {
-  _analyzeTone: function (speech, callback) {
-    var tone_analyzer = watson.tone_analyzer({
-      url: "https://gateway.watsonplatform.net/tone-analyzer/api",
-      username: '67de49fe-9c37-4cf6-9ebb-f0e5be00533d',
-      password: 'mz5qSSNJQupj',
-      version: 'v3',
-      version_date: '2016-05-19 '
-    });
-
-    tone_analyzer.tone({text: req.body.transcript}, function (err, tone) {
-      if (err) {
-        console.log(err);
-        callback(err); 
-      } else {
-        res = JSON.stringify(tone, null, 2)
-        console.log(res); 
-        callback(null, res); 
-      }
-    })
-  },
-  
   createSpeech: function (req, res) {
     /*** Calls _analyzTone ***/
     console.log('createSpeech'); 
-    const speech = req.body.transcript
-    this._analyzeTone(speech, function (err, data) {
+    const speech = req.body.transcript;
+    console.log(typeof this._analyzeTone); 
+    _analyzeTone(speech, function (err, data) {
       if (err) {
         res.statusCode(500).send(); 
       } else {
         var speechObj = {
           practice_id: req.body.practiceId,
-          session_id: req.body.sessionId,
+          sessionId: req.body.sessionId,
           analysis: data
         }
         return new Speech(speechObj).save()
--- server/models/SpeechModel.js
+++ server/models/SpeechModel.js
@@ -8,6 +8,19 @@ db.knex.schema.hasTable('speeches').then(function(exists){
     db.knex.schema.createTable('speeches', function(speech) {
       speech.increments('id').primary();
       speech.string('analysis'); 
+      // speech.string('disgust'); 
+      // speech.string('fear'); 
+      // speech.string('joy'); 
+      // speech.string('sadness'); 
+      // speech.string('language-analytical');
+      // speech.string('language-confident'); 
+      // speech.string('language-tentative'); 
+      // speech.string('openness_big5'); 
+      // speech.string('onscientiousness_big5'); 
+      // speech.string('extraversion_big5'); 
+      // speech.string('agreeableness_big5'); 
+      // speech.string('emotional_range_big5'); 
+
       speech.integer('userId').unsigned().references('users.id');;
       speech.integer('sessionId').unsigned().references('sessions.id');;
       speech.integer('practice_id').unsigned().references('practices.id');
