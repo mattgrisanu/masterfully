f4208fab76877749f91d83b23fad3d507c5139fd
Grisanu Naing <mgrisanu@gmail.com>
2016-06-10 21:02:20 -0700

    (doc) Update README

--- server/controllers/SpeechController.js
+++ server/controllers/SpeechController.js
@@ -1,29 +1,23 @@
 var Speech = require('../models/SpeechModel.js');
 var watson = require('watson-developer-cloud'); 
 
-// Load environment variables
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: './env/development.env' });
-} else if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: './env/production.env' });
-}
-
-var _analyzeTone = function (speech, callback) {
-  var tone_analyzer = watson.tone_analyzer({
+var _tone_analyzer = watson.tone_analyzer({
     url: "https://gateway.watsonplatform.net/tone-analyzer/api",
     username: process.env.TA_USER,
     password: process.env.TA_PASSWORD,
     version: 'v3',
     version_date: '2016-05-19 '
-  });
+});
 
-  tone_analyzer.tone({text: speech}, function (err, res) {
+var _analyzeTone = function (speech, callback) {
+  _tone_analyzer.tone({text: speech}, function (err, res) {
     if (err) {
       console.log(err);
       callback(err); 
     } else {
       var resObj = {};
       var array = res.document_tone.tone_categories;
+      
       array.forEach(function(tone) {
         // console.log('tone',tone, typeof tone); 
         tone.tones.forEach(function(emotion) {
@@ -36,7 +30,7 @@ var _analyzeTone = function (speech, callback) {
 
       callback(null, resObj); 
     }
-  })
+  });
 }
 
 module.exports = {
